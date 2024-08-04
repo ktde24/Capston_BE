51bf43d0b0d14a52c8c0ef66762c2892e1b2bac4
lrenais <bleuing@ewhain.net>
2024-08-04 13:21:58 +0900

    특정 일기 조회.수정.삭제 성공(0804)

--- src/controllers/diaryController.js
+++ src/controllers/diaryController.js
@@ -92,13 +92,24 @@ const getAllDiaries=asyncHandler(async (req, res) => {
 //일기 id(/:userId/:diaryId)로 특정 일기 조회
 const getDiary=asyncHandler(async (req, res) => {
   const userId= req.params.userId;
-  const diaryId = req.params.diaryId;
+  const diaryId = req.params.diaryId.trim();//공백문자 제거
   try {  
-    const userObjectId = new mongoose.Types.ObjectId(userId);
-    //일기 찾기
-    const diary = await Diary.findOne({userId:userObjectId, _id:diaryId});
+    //사용자 확인
+    const user = await ElderlyUser.findOne({id:userId});
+    if (!user) {
+      res.status(500).json({ message: '사용자가 없습니다.' });
+      return;
+    }
+
+    // 일기 찾기
+    const diary = await Diary.findById(diaryId);
     if (!diary) {
-      res.status(500).json({ message: '일기를 찾을 수 없습니다.' });
+      res.status(404).json({ message: '일기를 찾을 수 없습니다.' });
+      return;
+    }
+    
+    if (!diary.userId.equals(user._id)) { // 일기는 존재하지만 다른 사용자의 일기인지 확인(objectId라서 equals로 비교)
+      res.status(403).json({ message: '접근 권한이 없는 일기입니다.' });
       return;
     }
 
@@ -112,10 +123,30 @@ const getDiary=asyncHandler(async (req, res) => {
 
 //일기 id(/:userId/:diaryId)로 일기 삭제
 const deleteDiary=asyncHandler(async (req, res) => {
-  const { userId, diaryId } = req.params;
+  const userId= req.params.userId;
+  const diaryId = req.params.diaryId.trim();//공백문자 제거
+
   try {
+    //사용자 확인
+    const user = await ElderlyUser.findOne({id:userId});
+    if (!user) {
+      res.status(500).json({ message: '사용자가 없습니다.' });
+      return;
+    }
+
+    // 일기 찾기
+    const diary = await Diary.findById(diaryId);
+    if (!diary) {
+      res.status(404).json({ message: '일기를 찾을 수 없습니다.' });
+      return;
+    }
+    if (!diary.userId.equals(user._id)) { // 일기는 존재하지만 다른 사용자의 일기인지 확인(objectId라서 equals로 비교)
+      res.status(403).json({ message: '접근 권한이 없는 일기입니다.' });
+      return;
+    }
+
     //일기 삭제
-    const result = await Diary.findOneAndDelete({ diaryId: diaryId, userId: userId });
+    const result = await Diary.findOneAndDelete({ userId: diary.userId, _id: diaryId });
 
     //삭제된 일기가 없는 경우
     if (!result) {
@@ -131,12 +162,34 @@ const deleteDiary=asyncHandler(async (req, res) => {
 
 //일기 id(/:userId/:diaryId)로 일기 수정
 const updateDiary=asyncHandler(async (req, res) => {
-  const { userId, diaryId } = req.params;
   const updatedContent = req.body.content; 
-  try {
+  const userId= req.params.userId;
+  const diaryId = req.params.diaryId.trim();//공백문자 제거
+  try {  
+    //사용자 확인
+    const user = await ElderlyUser.findOne({id:userId});
+    if (!user) {
+      res.status(500).json({ message: '사용자가 없습니다.' });
+      return;
+    }
+
+    // 일기 찾기
+    const diary = await Diary.findById(diaryId);
+    if (!diary) {
+      res.status(404).json({ message: '일기를 찾을 수 없습니다.' });
+      return;
+    }
+    
+    if (!diary.userId.equals(user._id)) { // 일기는 존재하지만 다른 사용자의 일기인지 확인(objectId라서 equals로 비교)
+      console.log(diary.userId);
+      console.log(user._id);
+      res.status(403).json({ message: '접근 권한이 없는 일기입니다.' });
+      return;
+    }
+
     //일기 수정
     const result = await Diary.findOneAndUpdate(
-      { userId: userId, diaryId: diaryId },
+      { userId: diary.userId, _id: diaryId },
       { content: updatedContent },
       { new: true } //new:업데이트된 문서 반환
     );
@@ -150,6 +203,7 @@ const updateDiary=asyncHandler(async (req, res) => {
     
   } catch (error) {
     res.status(500).json({ message: '일기 수정 실패' });
+    console.log(error);
   }
 });
 
