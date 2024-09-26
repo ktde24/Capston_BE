75d284bd3b4f2e48c0fc2ac96aeeca67700ac2d3
ktde24 <kjjgbjbe24@gmail.com>
2024-09-26 21:37:35 +0900

    push알림 모델 코드 추가 & 컨트롤러 수정

--- src/controllers/pushController.js
+++ src/controllers/pushController.js
@@ -1,57 +1,60 @@
 const {messaging}=require('../utils/firebaseConfig');
+const Alarm = require('../models/alarm');
 const schedule = require('node-schedule');
-const ElderlyUser = require('../models/ElderlyUser');
+//const ElderlyUser = require('../models/ElderlyUser');
 const asyncHandler = require('express-async-handler');
 
 const sendPushNotice= asyncHandler(async (req, res) =>{
-  const { id } = req.params;
-  const elderly = await ElderlyUser.findOne({ id });
+  const userId = req.user._id;  // JWT 인증을 통해 userId 추출
+  const alarm = await Alarm.findOne({ userId: id });
+  //const elderly = await ElderlyUser.findOne({ id });
   
   //사용자가 설정해놓은 시간 db에서 가져오기(elderly table에 속성 추가해야됨)
   //let hour=elderly.pushHour ?? 17;
   //let minute=elderly.pushMin ?? 0;
 
-  let hour=17;
-  let minute=0;
+  if (!alarm) {
+    return res.status(404).json({ message: '알림 설정을 찾을 수 없습니다.' });
+  }
 
-  const registrationToken='Registration token from the client';
+  let hour = alarm.hour ?? 17;
+  let minute = alarm.minute ?? 0;
+
+  const registrationToken = 'Registration token from the client'; // 사용자별로 토큰을 가져와야 함
 
   try {
-    const message={
+    const message = {
       notification: {
         title: '일기를 작성할 시간이에요!',
       },
-      android: { //android 기기에 전송
+      android: {
         notification: {
           icon: 'stock_ticker_update',
           color: '#7e55c3',
-          //clickAction:'news_intent' : 이 알림 누르면 news_intent 실행
         }
       },
-      /*webpush: {//눌렀을 때 웹페이지와 연결
-        fcmOptions: {
-          link: 'breakingnews.html'
-        }
-      },*/
       tokens: registrationToken,
       topic: '일기 작성 알림',
     };
-    
-    let pushNotice=schedule.scheduleJob(`${minute} ${hour} * * *`,function(){
-      console.log(`${minute}시 ${hour}에 실행`);
-      messaging().send(message).then((response)=>{
-        //response: msg ID string
-        console.log('메시지 알림 성공',response);
-      })
-      .catch((error)=>{
-        console.log('메시지 알림 전송 중 에러 발생',error);
-      });  
+
+    // 일정 시간에 푸시 알림을 스케줄링
+    let pushNotice = schedule.scheduleJob(`${minute} ${hour} * * *`, function() {
+      console.log(`${minute}분 ${hour}시에 알림 전송`);
+      messaging().send(message)
+        .then((response) => {
+          console.log('메시지 알림 전송 성공', response);
+        })
+        .catch((error) => {
+          console.log('메시지 알림 전송 중 에러 발생', error);
+        });
     });
 
+    res.status(200).json({ message: '알림 전송이 스케줄링되었습니다.' });
+
   } catch (error) {
-    res.status(500).json({ message: '메시지 알림 전송 중 에러 발생' });
+    console.error('푸시 알림 전송 중 에러 발생:', error);
+    res.status(500).json({ message: '메시지 알림 전송 중 에러 발생', error });
   }
 });
 
-module.exports={sendPushNotice};
-
+module.exports = { sendPushNotice };
---
+++ src/models/alarm.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+// 알림(Push) 스키마 정의
+const AlarmSchema = new mongoose.Schema({
+  pushId: { 
+    type: Number, 
+    required: true, 
+    unique: true 
+  },  // 푸시 알림 고유 ID
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },  // 사용자 ID, User 모델 참조
+  hour: { 
+    type: Number, 
+    required: true 
+  },  // 알림 설정 시간 (시)
+  minute: { 
+    type: Number, 
+    required: true 
+  },  // 알림 설정 시간 (분)
+}, {
+  timestamps: true  // 생성 시간과 수정 시간을 자동으로 기록
+});
+
+const Alarm = mongoose.model('Alarm', AlarmSchema);
+
+module.exports = Alarm;
